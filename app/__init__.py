a22593a80a23be066cbd154261c1f4d05472c2b1
Léonard Gagnon <leonard.gagnon@gmail.com>
2017-09-06 16:54:52 -0400

    Better config for flask + templates base

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+app.config.from_object('config.default')
+app.config.from_envvar('APP_CONFIG', silent=True)
+
 from app import views
--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!flask/bin/python
 from app import app
+import webbrowser
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    if not app.config['DEBUG']:
+        webbrowser.open('http://localhost:5000')
     app.run()
